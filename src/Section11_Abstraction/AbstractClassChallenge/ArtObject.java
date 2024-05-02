01081c5ce405e75ae2166f9afbf6bae01bf3e0d4
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-05-02 17:08:07 +0200

    Abstract classes challenge

---
+++ src/Section11_Abstraction/AbstractClassChallenge/ArtObject.java
@@ -0,0 +1,15 @@
+package Section11_Abstraction.AbstractClassChallenge;
+
+public class ArtObject extends ProductForSale{
+
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " is a beutiful reproduction");
+        System.out.printf("The price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
+}
