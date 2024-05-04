0a46c4d1a45496be9889030d399e37423e5fd179
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-05-04 16:32:09 +0200

    interfaces part 1

---
+++ src/Section11_Abstraction/Interfaces/Main.java
@@ -0,0 +1,32 @@
+package Section11_Abstraction.Interfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        Animal animal = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
+
+        animal.move();
+//        flier.move();
+//        tracked.move();
+
+//        flier.takeOff();
+//        flier.fly();
+//        tracked.track();
+//        flier.land();
+
+        inFlight(flier);
+    }
+
+    private static void inFlight(FlightEnabled flier) {
+        flier.takeOff();
+        flier.fly();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
+        flier.land();
+    }
+}
