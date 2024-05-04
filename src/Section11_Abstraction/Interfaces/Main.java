e60295ea8651052e2f18dcb1b40f6113b525f273
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-05-04 17:03:51 +0200

    interfaces part 3

--- src/Section11_Abstraction/Interfaces/Main.java
+++ src/Section11_Abstraction/Interfaces/Main.java
@@ -1,5 +1,9 @@
 package Section11_Abstraction.Interfaces;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +30,20 @@ public class Main {
         double kmsTraveled = 100;
         double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
+
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -35,5 +53,23 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+
+    private static void triggerFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 }
