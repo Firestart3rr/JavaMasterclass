7e471be593e9ef301797b77eb497ff2f48127f96
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-09-28 11:56:13 +0200

    Lambda Expressions, syntax and aditional examples

---
+++ src/Section14_LambdaExpressionsFunctionalInterfacesAndMethodReferences/LambdaExpressions.java
@@ -0,0 +1,28 @@
+package Section14_LambdaExpressionsFunctionalInterfacesAndMethodReferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaExpressions {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie", "delta"
+        ));
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("-----------");
+        list.forEach(myString -> System.out.println(myString));
+
+        System.out.println("-----------");
+        String prefix = "nato";
+        list.forEach((var myString) -> {
+            char first = myString.charAt(0);
+            System.out.println(prefix + " " + myString + " means " + first);
+        });
+    }
+}
