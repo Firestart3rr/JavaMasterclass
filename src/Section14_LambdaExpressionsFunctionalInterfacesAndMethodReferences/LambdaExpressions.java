a8a4c14e52d5c5f1817a6815e7310c60d0e683c8
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-10-03 16:13:18 +0200

    Java's Functional Interfaces, Function & Supplier

--- src/Section14_LambdaExpressionsFunctionalInterfacesAndMethodReferences/LambdaExpressions.java
+++ src/Section14_LambdaExpressionsFunctionalInterfacesAndMethodReferences/LambdaExpressions.java
@@ -3,8 +3,10 @@ package Section14_LambdaExpressionsFunctionalInterfacesAndMethodReferences;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 
 public class LambdaExpressions {
 
@@ -60,6 +62,32 @@ public class LambdaExpressions {
         System.out.println("------------");
         list.removeIf(s -> s.startsWith("ea"));
         list.forEach(s -> System.out.println(s));
+
+        list.replaceAll(s -> s.charAt(0) + " - " + s.toUpperCase());
+        System.out.println("------------");
+        list.forEach(s -> System.out.println(s));
+
+        String[] emptyStrings = new String[10];
+        System.out.println(Arrays.toString(emptyStrings));
+        Arrays.fill(emptyStrings, "");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". ");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". "
+                        + switch (i) {
+                    case 0 -> "one";
+                    case 1 -> "two";
+                    case 2 -> "three";
+                    default -> "";
+                }
+        );
+        System.out.println(Arrays.toString(emptyStrings));
+
+        String[] names = {"Ann", "Bob", "Carol", "David", "Ed", "Fred"};
+        String[] randomList = randomlySelectedValues(15, names, () -> new Random().nextInt(0, names.length));
+        System.out.println(Arrays.toString(randomList));
     }
 
 
@@ -79,5 +107,13 @@ public class LambdaExpressions {
 
     public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
         consumer.accept(t1, t2);
+    }
+
+    public static String[] randomlySelectedValues(int count, String[] values, Supplier<Integer> s) {
+        String[] selectedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            selectedValues[i] = values[s.get()];
+        }
+        return selectedValues;
     }
 }
