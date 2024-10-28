37c59ecdf46d559f3f68574c0e45a1a2ea1a6c9f
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-10-28 19:33:15 +0100

    Deck of Cards for Collections methods

---
+++ src/Section15_JavaCollections/CollectionsMethodsCardsDeck/Main.java
@@ -0,0 +1,12 @@
+package Section15_JavaCollections.CollectionsMethodsCardsDeck;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
