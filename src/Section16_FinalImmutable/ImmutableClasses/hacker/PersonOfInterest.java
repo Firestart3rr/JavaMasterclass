b9977724e62ca58fae44001c8016bb22309d83ec
Jakub Wziatka <kuba.wziatka@gmail.com>
2024-12-28 16:40:16 +0100

    declaring immutable classes to produce immutable objects

---
+++ src/Section16_FinalImmutable/ImmutableClasses/hacker/PersonOfInterest.java
@@ -0,0 +1,10 @@
+package Section16_FinalImmutable.ImmutableClasses.hacker;
+
+import Section16_FinalImmutable.ImmutableClasses.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+}
