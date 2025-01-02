67b28a115c92698a2c2a5e1adcb604198e9449f0
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-02 15:45:14 +0100

    immutable classes challenge

---
+++ src/Section16_FinalImmutable/ChallengeImmutableClasses/Main.java
@@ -0,0 +1,26 @@
+package Section16_FinalImmutable.ChallengeImmutableClasses;
+
+import Section16_FinalImmutable.ChallengeImmutableClasses.bank.BankAccount;
+import Section16_FinalImmutable.ChallengeImmutableClasses.bank.BankCustomer;
+
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+//        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING, 500);
+//        System.out.println(account);
+
+        BankCustomer joe = new BankCustomer("Joe", 500.00, 10000.00);
+        System.out.println(joe);
+
+        List<BankAccount> accounts = joe.getAccounts();
+        accounts.clear();
+        System.out.println(joe);
+
+//        accounts.add(new BankAccount(BankAccount.AccountType.CHECKING, 150000));
+//        System.out.println(joe);
+    }
+
+}
