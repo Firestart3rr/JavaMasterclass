8df10c17549ba55179e2036369fe5cb79490d1e2
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-06 11:57:22 +0100

    Record Constructors and javap

---
+++ src/Section16_FinalImmutable/ConstructorsProject/Person.java
@@ -0,0 +1,18 @@
+package Section16_FinalImmutable.ConstructorsProject;
+
+public record Person(String name, String dob) {
+
+//    public Person(String name, String dob) {
+//        this.name = name;
+//        this.dob = dob.replace('-', '/');
+//    }
+
+    public Person(Person p) {
+        this(p.name, p.dob);
+    }
+
+    public Person {
+        if (dob == null) throw new IllegalArgumentException("bad data");
+        dob = dob.replace('-', '/');
+    }
+}
