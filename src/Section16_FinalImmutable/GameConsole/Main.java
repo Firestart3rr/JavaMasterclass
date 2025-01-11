d9aa8d81bb58a31d71da3ea70f5f1cceca219476
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-11 13:02:54 +0100

    Game console setup, part 2

--- src/Section16_FinalImmutable/GameConsole/Main.java
+++ src/Section16_FinalImmutable/GameConsole/Main.java
@@ -1,8 +1,15 @@
 package Section16_FinalImmutable.GameConsole;
 
+import Section16_FinalImmutable.GameConsole.game.GameConsole;
+import Section16_FinalImmutable.GameConsole.game.ShooterGame;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+
+        int playerIndex = console.addPlayer();
+        console.playGame(playerIndex);
     }
 }
