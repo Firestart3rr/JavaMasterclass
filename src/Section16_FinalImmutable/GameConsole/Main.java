f573a641c1c011f9e9f004d8d18932a2d1dc2736
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-25 10:28:28 +0100

    Challenge: Initializers and Constructors, A Pirate Game

--- src/Section16_FinalImmutable/GameConsole/Main.java
+++ src/Section16_FinalImmutable/GameConsole/Main.java
@@ -1,15 +1,24 @@
 package Section16_FinalImmutable.GameConsole;
 
-import Section16_FinalImmutable.GameConsole.game.GameConsole;
-import Section16_FinalImmutable.GameConsole.game.ShooterGame;
+import Section16_FinalImmutable.GameConsole.pirate.Pirate;
+import Section16_FinalImmutable.GameConsole.pirate.Weapon;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//        var console = new GameConsole<>(new ShooterGame("The Shootout Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);l
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println("weapon = " + weapon + ", hitPoints=" + weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
+
+        var list = Weapon.getWeaponByLevel(1);
+        list.forEach(System.out::println);
+
+        Pirate tim = new Pirate("Tim");
+        System.out.println(tim);
     }
 }
