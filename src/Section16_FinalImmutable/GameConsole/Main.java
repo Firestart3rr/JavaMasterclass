1c0582ff4eedbd25bd0db1b67f01ae9eb6b41a22
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-25 11:30:35 +0100

    Challenge: Pirate Game continued

--- src/Section16_FinalImmutable/GameConsole/Main.java
+++ src/Section16_FinalImmutable/GameConsole/Main.java
@@ -1,6 +1,8 @@
 package Section16_FinalImmutable.GameConsole;
 
+import Section16_FinalImmutable.GameConsole.game.GameConsole;
 import Section16_FinalImmutable.GameConsole.pirate.Pirate;
+import Section16_FinalImmutable.GameConsole.pirate.PirateGame;
 import Section16_FinalImmutable.GameConsole.pirate.Weapon;
 
 public class Main {
@@ -20,5 +22,13 @@ public class Main {
 
         Pirate tim = new Pirate("Tim");
         System.out.println(tim);
+
+        PirateGame.getTowns(0).forEach(System.out::println);
+        System.out.println("-----------------------------------------------");
+        PirateGame.getTowns(1).forEach(System.out::println);
+
+        var console = new GameConsole<>(new PirateGame("The Pirate Game"));
+        int playerIndex = console.addPlayer();
+        console.playGame(playerIndex);
     }
 }
