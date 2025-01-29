482a4d7a0590f803cfa81d737499c9ea77af54e2
Jakub Wziatka <kuba.wziatka@gmail.com>
2025-01-29 20:22:23 +0100

    Final Section Challenge

--- src/Section16_FinalImmutable/GameConsole/Main.java
+++ src/Section16_FinalImmutable/GameConsole/Main.java
@@ -14,18 +14,18 @@ public class Main {
 //        int playerIndex = console.addPlayer();
 //        console.playGame(playerIndex);l
 
-        Weapon weapon = Weapon.getWeaponByChar('P');
-        System.out.println("weapon = " + weapon + ", hitPoints=" + weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
-
-        var list = Weapon.getWeaponByLevel(1);
-        list.forEach(System.out::println);
-
-        Pirate tim = new Pirate("Tim");
-        System.out.println(tim);
-
-        PirateGame.getTowns(0).forEach(System.out::println);
-        System.out.println("-----------------------------------------------");
-        PirateGame.getTowns(1).forEach(System.out::println);
+//        Weapon weapon = Weapon.getWeaponByChar('P');
+//        System.out.println("weapon = " + weapon + ", hitPoints=" + weapon.getHitPoints() + ", minLevel=" + weapon.getMinLevel());
+//
+//        var list = Weapon.getWeaponByLevel(1);
+//        list.forEach(System.out::println);
+//
+//        Pirate tim = new Pirate("Tim");
+//        System.out.println(tim);
+//
+//        PirateGame.getTowns(0).forEach(t -> System.out.println(t.information()));
+//        System.out.println("-----------------------------------------------");
+//        PirateGame.getTowns(1).forEach(t -> System.out.println(t.information()));
 
         var console = new GameConsole<>(new PirateGame("The Pirate Game"));
         int playerIndex = console.addPlayer();
